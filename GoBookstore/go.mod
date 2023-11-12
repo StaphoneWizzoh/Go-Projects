8e86ce07b184e9a9923f4fd259a3ad3dd45e04b7
StaphoneWizzoh <95640951+StaphoneWizzoh@users.noreply.github.com>
2023-11-12 18:33:58 +0300

    Tried  sqlite3
    Not the perfect implementation so far.

--- GoBookstore/go.mod
+++ GoBookstore/go.mod
@@ -3,8 +3,12 @@ module GoBookstore
 go 1.21
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
--- GoBookstore/pkg/config/app.go
+++ GoBookstore/pkg/config/app.go
@@ -1,8 +1,32 @@
+//package config
+//
+//import (
+//	_ "github.com/go-sql-driver/mysql"
+//	"github.com/jinzhu/gorm"
+//)
+//
+//var (
+//	db *gorm.DB
+//)
+//
+//func Connect() {
+//	dsn := "root:staphone@tcp(127.0.0.1:3306)/bookstore"
+//	d, err := gorm.Open("mysql", dsn)
+//	if err != nil {
+//		panic(err)
+//	}
+//	db = d
+//}
+//
+//func GetDB() *gorm.DB {
+//	return db
+//}
+
 package config
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/inflection"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var (
@@ -10,7 +34,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:staphone/goBookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
