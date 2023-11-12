b647b4a6bdd8801535f426923050eb2c72706fe4
StaphoneWizzoh <95640951+StaphoneWizzoh@users.noreply.github.com>
2023-11-12 21:44:01 +0300

    Db changes

--- GoBookstore/pkg/config/app.go
+++ GoBookstore/pkg/config/app.go
@@ -1,8 +1,32 @@
+package config
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "root:staphone@tcp(127.0.0.1:3306)/bookstore"
+	d, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+//func GetDB() *gorm.DB {
+//	return db
+//}
+
 //package config
 //
 //import (
-//	_ "github.com/go-sql-driver/mysql"
-//	"github.com/jinzhu/gorm"
+//	"gorm.io/driver/sqlite"
+//	"gorm.io/gorm"
 //)
 //
 //var (
@@ -10,8 +34,7 @@
 //)
 //
 //func Connect() {
-//	dsn := "root:staphone@tcp(127.0.0.1:3306)/bookstore"
-//	d, err := gorm.Open("mysql", dsn)
+//	d, err := gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
 //	if err != nil {
 //		panic(err)
 //	}
@@ -21,26 +44,3 @@
 //func GetDB() *gorm.DB {
 //	return db
 //}
-
-package config
-
-import (
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
-var (
-	db *gorm.DB
-)
-
-func Connect() {
-	d, err := gorm.Open(sqlite.Open("bookstore.db"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	db = d
-}
-
-func GetDB() *gorm.DB {
-	return db
-}
